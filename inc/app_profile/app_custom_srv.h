96033cdb246964276f8e01d4f6d1e85fe839d2c9
xianii <jiyucheng007@gmail.com>
2021-12-08 18:20:55 +0800

    change service uuid

--- inc/app_profile/app_custom_srv.h
+++ inc/app_profile/app_custom_srv.h
@@ -1,36 +1,8 @@
-/*****************************************************************************
- * Copyright (c) 2019, Nations Technologies Inc.
- *
- * All rights reserved.
- * ****************************************************************************
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the disclaimer below.
- *
- * Nations' name may not be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY NATIONS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
- * DISCLAIMED. IN NO EVENT SHALL NATIONS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * ****************************************************************************/
 
 /**
  * @file app_custom_srv.h
- * @author Nations Firmware Team
+ * @author
  * @version v1.0.0
- *
- * @copyright Copyright (c) 2019, Nations Technologies Inc. All rights reserved.
  */
 
 
@@ -57,9 +29,9 @@
 
 
 
-#define ATT_SERVICE_AM_SPEED_16          0xfffa           /*!< Service UUID */
-#define ATT_CHAR_AM_SPEED_WRITE_16       0xfffb           /*!< Characteristic value UUID */
-#define ATT_CHAR_AM_SPEED_NTF_16         0xfffc           /*!< Characteristic value UUID */
+#define ATT_SERVICE_AM_SPEED_16          0xCC00           /*!< Service UUID */
+#define ATT_CHAR_AM_SPEED_WRITE_16       0xCC01           /*!< Characteristic value UUID */
+#define ATT_CHAR_AM_SPEED_NTF_16         0xCC02           /*!< Characteristic value UUID */
 
 /// rdtss 16bit uuid Service Attributes Indexes
 enum {
